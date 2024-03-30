9cc7a99aeaf59808d2128f57cc6fbf704dfb8b5c
Mateusz <m.molenda4@gmail.com>
2024-03-30 20:00:03 +0100

    Update

--- startup.sh
+++ startup.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+set -e
 
+docker compose -f compose.yaml down
 
 # Build images
-docker build -f Dockerfile_front . -t chmiel_front
+if [[ $# -ge 1 ]] ; then
+        docker build -f Dockerfile_front . -t chmiel_front
+        docker build -f Dockerfile_db . -t chmiel_db
+#       docker build -f Dockerfile_back . -t chmiel_back
+fi
 
-docker build -f Dockerfile_back . -t chmiel_back
 
+docker rm -f chmiel_back
+docker rm -f chmiel_front
+docker rm -f chmiel_db
 
 # Build compose
 docker compose -f compose.yaml build
 
 # Run compose
-docker compose -f compose.yaml up -d
-
+docker compose -f compose.yaml up -d --remove-orphans
 
 # Check if Docker Compose was successful
-if [ $? -ne 0 ]; then
-    echo "Docker Compose failed. Saving logs to 'failed_compose.log'"
-    docker compose -f compose.yaml logs > failed_compose.log
-else
-    echo "Docker Compose was successful."
-fi
+echo "Waiting for docker to start.."
+sleep 5
+
+
+docker compose -f compose.yaml logs > log_compose
+docker compose -f compose.yaml logs
+set +e
+docker ps
+
+
+
