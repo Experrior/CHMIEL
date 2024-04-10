7359afb19c7768d556c1c5a17a9ea043fa99938f
djodaswiatla <zu.aszkielowicz@gmail.com>
2024-04-10 19:18:57 +0200

    simplified dao repositories

--- chmiel/src/main/java/com/backend/chmiel/dao/GroupRepository.java
+++ chmiel/src/main/java/com/backend/chmiel/dao/GroupRepository.java
@@ -1,8 +1,6 @@
 package com.backend.chmiel.dao;
 
 import com.backend.chmiel.entity.Group;
-import com.backend.chmiel.entity.User;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -11,10 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface GroupRepository extends JpaRepository<Group, Integer> {
     
     Optional<Group> findByGroupName(String groupName);
-
-    List<Group> findAllByGroupUsersUserId(Integer userId);
-
-    List<Group> findAllByGroupUsersUser(User user);
 
     List<Group> findAllByGroupNameContaining(String keyword);
 
--- chmiel/src/main/java/com/backend/chmiel/dao/SprintRepository.java
+++ chmiel/src/main/java/com/backend/chmiel/dao/SprintRepository.java
@@ -5,20 +5,13 @@ import com.backend.chmiel.entity.Sprint;
 
 import java.sql.Timestamp;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SprintRepository extends JpaRepository<Sprint, Integer> {
-    
-    Optional<Sprint> findBySprintNameAndGroupId(String sprintName, Integer groupId);
 
     List<Sprint> findAllByGroup(Group group);
 
-    // sprints with a start time after a specified time
     List<Sprint> findAllByStartTimeAfterOrderByStartTimeAsc(Timestamp startTime);
 
-    // sprints with a start time before a specified time
     List<Sprint> findAllByStopTimeBeforeOrderByStopTimeAsc(Timestamp stopTime);
-    
 }
