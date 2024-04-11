a98061ecedcf49ac40ac2963b7f6f3c87a1dd01f
zuzaszk <zu.aszkielowicz@gmail.com>
2024-04-11 19:00:38 +0200

    changed naming convention to snake_case

--- db_init/create_tables.sql
+++ db_init/create_tables.sql
@@ -10,51 +10,51 @@ CREATE TABLE IF NOT EXISTS USERS (
 
 -- Create Table GROUPS
 CREATE TABLE IF NOT EXISTS GROUPS (
-  groupId SERIAL PRIMARY KEY,
-  groupName VARCHAR UNIQUE NOT NULL
+  group_id SERIAL PRIMARY KEY,
+  group_name VARCHAR UNIQUE NOT NULL
 );
 
 -- Create Table GROUP_USERS
 CREATE TABLE IF NOT EXISTS GROUP_USERS (
   user_id INTEGER,
-  groupId INTEGER,
-  PRIMARY KEY (user_id, groupId),
+  group_id INTEGER,
+  PRIMARY KEY (user_id, group_id),
   FOREIGN KEY (user_id) REFERENCES USERS(user_id),
-  FOREIGN KEY (groupId) REFERENCES GROUPS(groupId)
+  FOREIGN KEY (group_id) REFERENCES GROUPS(group_id)
 );
 
 -- Create Table SPRINTS
 CREATE TABLE IF NOT EXISTS SPRINTS (
-  sprintId SERIAL PRIMARY KEY,
-  sprintName VARCHAR NOT NULL,
-  groupId INTEGER REFERENCES GROUPS(groupId),
-  startTime TIMESTAMP NOT NULL,
-  stopTime TIMESTAMP NOT NULL,
+  sprint_id SERIAL PRIMARY KEY,
+  sprint_name VARCHAR NOT NULL,
+  group_id INTEGER REFERENCES GROUPS(group_id),
+  start_time TIMESTAMP NOT NULL,
+  stop_time TIMESTAMP NOT NULL,
   logged BOOLEAN NOT NULL,
-  CONSTRAINT unique_sprint_groupName UNIQUE (sprintName, groupId)
+  CONSTRAINT unique_sprint_group_name UNIQUE (sprint_name, group_id)
 );
 
 -- Create Table TASKS
 CREATE TABLE IF NOT EXISTS TASKS (
-  taskId SERIAL PRIMARY KEY,
-  asigneeId INTEGER REFERENCES USERS(user_id),
-  reportedId INTEGER REFERENCES USERS(user_id),
-  groupId INTEGER REFERENCES GROUPS(groupId),
-  sprintId INTEGER REFERENCES SPRINTS(sprintId),
+  task_id SERIAL PRIMARY KEY,
+  asignee_id INTEGER REFERENCES USERS(user_id),
+  reported_id INTEGER REFERENCES USERS(user_id),
+  group_id INTEGER REFERENCES GROUPS(group_id),
+  sprint_id INTEGER REFERENCES SPRINTS(sprint_id),
   name VARCHAR NOT NULL,
   description VARCHAR,
-  loggedHours FLOAT,
-  timeEstimate FLOAT NOT NULL,
+  logged_hours FLOAT,
+  time_estimate FLOAT NOT NULL,
   status VARCHAR CHECK (status IN ('backlog', 'open', 'in progress', 'review', 'closed')),
-  isEpic BOOLEAN NOT NULL,
-  inEpic INTEGER REFERENCES TASKS(taskId)
+  is_epic BOOLEAN NOT NULL,
+  in_epic INTEGER REFERENCES TASKS(task_id)
 );
 
 -- Create Table TASK_COMMENTS
 CREATE TABLE IF NOT EXISTS TASK_COMMENTS (
-  taskCommentId SERIAL PRIMARY KEY,
-  taskId INTEGER REFERENCES TASKS(taskId),
+  task_comment_id SERIAL PRIMARY KEY,
+  task_id INTEGER REFERENCES TASKS(task_id),
   message VARCHAR NOT NULL,
-  authorId INTEGER REFERENCES USERS(user_id),
+  author_id INTEGER REFERENCES USERS(user_id),
   logged BOOLEAN NOT NULL
 );
