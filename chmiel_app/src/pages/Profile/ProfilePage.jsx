fd3f1792e0be112e53a7840d6e6aeb515dfc4911
do-ko <doko@onet.eu>
2024-04-16 14:57:13 +0200

    - pulling data from db to display

--- chmiel_app/src/pages/Profile/ProfilePage.jsx
+++ chmiel_app/src/pages/Profile/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import {Button, Col, Row, Stack} from "react-bootstrap";
+import {Button, Col, Form, Modal, Row, Stack} from "react-bootstrap";
 import {Navigation} from "../../components/Navigation/Navigation";
 import "./ProfilePage.css"
 import default_profile_picture from "../../assets/default_profile_picture.jpg"
@@ -8,11 +8,144 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import {ProjectComponent} from "../../components/ProfileComponents/ProjectComponent";
 import useScreenSize from "../../other/useScreenSize";
+import axios from "../../api/axios";
+import {useCookies} from "react-cookie";
 
 export const ProfilePage = () => {
+    const [cookies] = useCookies(["token"]);
     const screenSize = useScreenSize();
     const [columnNum, setColumnNum] = useState(0)
-    const [accountDetails, setAccountDetails] = useState({name: "Jane Doe"})
+    const [accountDetails, setAccountDetails] = useState({})
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [address, setAddress] = useState("")
+    const [birthDate, setBirthDate] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+
+    // EditUserDetailsModal
+    const [modalShow, setModalShow] = useState(false);
+    const [validated, setValidated] = useState(false);
+
+    function EditUserDetailsModal(props) {
+        return (
+            <Modal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                backdrop={"static"}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                keyboard={true}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Edit User Details
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form noValidate validated={validated} onSubmit={onFormSubmit}>
+                        {/*<Row>*/}
+                        {/*    <Col>*/}
+                        {/*        /!*<Form.Group className="mb-3" controlId="formGroupFirstName">*!/*/}
+                        {/*        /!*    <Form.Label>First Name</Form.Label>*!/*/}
+                        {/*        /!*    <Form.Control required*!/*/}
+                        {/*        /!*                  type="text"*!/*/}
+                        {/*        /!*                  placeholder="FirstName"*!/*/}
+                        {/*        /!*                  onChange={onInputFirstName}*!/*/}
+                        {/*        /!*                  value={firstName}*!/*/}
+                        {/*        /!*    />*!/*/}
+                        {/*        /!*    <Form.Control.Feedback type="invalid">*!/*/}
+                        {/*        /!*        Please enter first name.*!/*/}
+                        {/*        /!*    </Form.Control.Feedback>*!/*/}
+                        {/*        /!*</Form.Group>*!/*/}
+                        {/*    </Col>*/}
+                        {/*    <Col>*/}
+                        {/*        <Form.Group className="mb-3" controlId="formGroupFirstName">*/}
+                        {/*            <Form.Label>Last Name</Form.Label>*/}
+                        {/*            <Form.Control required*/}
+                        {/*                          type="text"*/}
+                        {/*                          placeholder="LastName"*/}
+                        {/*                          onChange={(e) => {*/}
+                        {/*                              setLastName(e.target.value)*/}
+                        {/*                              setValidated(false)*/}
+                        {/*                          }*/}
+                        {/*                          }*/}
+                        {/*                          value={lastName}*/}
+                        {/*            />*/}
+                        {/*            <Form.Control.Feedback type="invalid">*/}
+                        {/*                Please enter last name.*/}
+                        {/*            </Form.Control.Feedback>*/}
+                        {/*        </Form.Group>*/}
+                        {/*    </Col>*/}
+                        {/*</Row>*/}
+                        <Form.Group className={"mb-3"} controlId={"formGroupEmail"}>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control required
+                                          type={"email"}
+                                          placeholder={"Email"}
+                                          onChange={onInputEmail}
+                                          value={email}
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={props.onHide}>Close</Button>
+                    <Button variant="primary" onClick={() => setModalShow(false)}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
+    const getUserDetails = async () => {
+        return await axios.get("/api/user", {
+            headers: {
+                Authorization: "Bearer " + cookies.token
+            }
+        })
+    }
+
+    const onFormSubmit = (e) => {
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+            setValidated(true);
+
+        } else {
+            e.preventDefault();
+            e.stopPropagation();
+            setValidated(false);
+
+            // if (shipping) {
+            //     setPayment(true);
+            //     setShipping(false);
+            // } else if (payment) {
+            //     setPayment(false);
+            //     setReview(true);
+            // }
+        }
+    }
+
+    const onInputEmail = ({target: {value}}) => {
+        setEmail(value)
+        // setValidated(false)
+        console.log("test")
+    }
+
+
+    useEffect(() => {
+        getUserDetails().then(results => {
+            console.log(results.data)
+            setAccountDetails(results.data)
+        }).catch(e => {
+            console.error(e);
+        })
+    }, [])
 
     const [projects, setProjects] = useState([
         {id: 1, name: "testProject1"},
@@ -87,32 +220,36 @@ export const ProfilePage = () => {
             <div className={"profileContainer"}>
                 <div className={"profileDetailsContainer"}>
                     <img className={"profilePicture"} src={default_profile_picture} alt={"default profile picture"}/>
-                    <h2>{accountDetails.name}</h2>
+                    <h2>{accountDetails.firstName} {accountDetails.lastName}</h2>
                     <div className={"profileDetails"}>
                         <div>
                             <p className={"profileDetailsHeader"}>Basic Details</p>
                             <div className={"profileDetail"}>
                                 <p>Date Of Birth</p>
-                                <p>Date Of Birth</p>
+                                {accountDetails.birthDate ? <p>{accountDetails.birthDate}</p> :
+                                    <p style={{color: "lightgrey", fontStyle: "italic"}}>undefined</p>}
                             </div>
                             <div className={"profileDetail"}>
                                 <p>Address</p>
-                                <p>Lake Gailmouth</p>
+                                {accountDetails.address ? <p>{accountDetails.address}</p> :
+                                    <p style={{color: "lightgrey", fontStyle: "italic"}}>undefined</p>}
                             </div>
                         </div>
                         <div>
                             <p className={"profileDetailsHeader"}>Contact Information</p>
                             <div className={"profileDetail"}>
                                 <p>Email</p>
-                                <p>test@email.com</p>
+                                <p>{accountDetails.email}</p>
                             </div>
                             <div className={"profileDetail"}>
                                 <p>Phone Number</p>
-                                <p>111 111 111</p>
+                                {accountDetails.phoneNumber ? <p>{accountDetails.phoneNumber}</p> :
+                                    <p style={{color: "lightgrey", fontStyle: "italic"}}>undefined</p>}
                             </div>
                         </div>
                     </div>
-                    <Button variant={"custom-primary"}>Edit details</Button>
+                    <Button variant={"custom-primary"} onClick={() => setModalShow(true)}>Edit details</Button>
+                    <EditUserDetailsModal/>
                 </div>
                 <div className={"profileProjects"}>
                     <Stack gap={3}>
@@ -154,9 +291,10 @@ export const ProfilePage = () => {
                                 {userConnections.length !== 0 ? <Row>
                                     {
                                         userConnections.map((userConnection) => {
-                                            return <Col xs={2} md={3} lg={2} xxxl={1}> <img style={{width: "80%", borderRadius: "50%", marginBottom: 16}}
-                                                                     src={default_profile_picture}
-                                                                     alt={"default profile picture"}/>
+                                            return <Col xs={2} md={3} lg={2} xxxl={1}> <img
+                                                style={{width: "80%", borderRadius: "50%", marginBottom: 16}}
+                                                src={default_profile_picture}
+                                                alt={"default profile picture"}/>
                                             </Col>
                                         })
                                     }
