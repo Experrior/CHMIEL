890ed1d774f924acb3131ebda9cb3757d893fea0
zuzaszk <zu.aszkielowicz@gmail.com>
2024-04-18 19:05:00 +0200

    project component details

--- chmiel_app/src/components/ProfileComponents/ProjectComponent.jsx
+++ chmiel_app/src/components/ProfileComponents/ProjectComponent.jsx
@@ -2,7 +2,12 @@ import './ProjectComponent.css'
 export const ProjectComponent = ({project}) => {
     return(
         <div key={project.id} className={"projectComponentContainer"}>
-            <h3>{project.name}</h3>
+            <h4>{project.name}</h4>
+            <div className={"quickLinks"}>
+                <h4>QUICK LINKS</h4>
+                <div className={"link"}><a href="">My open issues</a></div>
+                <div className={"link"}><a href="">Done issues</a></div>
+            </div>
         </div>
     )
 }
