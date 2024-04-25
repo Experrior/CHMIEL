69640361d2e3678735d3cf251e5ef6d99dc24fe9
zuzaszk <zu.aszkielowicz@gmail.com>
2024-04-25 20:16:51 +0200

    board screen (to be improved)

---
+++ chmiel_app/src/components/Board/BoardComponent.jsx
@@ -0,0 +1,28 @@
+import './BoardComponent.css'
+import { TaskComponent } from "../../components/Other/TaskComponent";
+import {useState} from "react";
+
+export const BoardComponent = ({panel}) => {
+
+    const [tasks, setTasks] = useState([
+        {id: 1, name: "testTask1"},
+        {id: 2, name: "testTask2"},
+        {id: 3, name: "testTask3"},
+        {id: 4, name: "testTask4"}
+    ])
+
+    return(
+        <div key={panel.id} className={"boardComponentContainer"}>
+            <h5>{panel.name}</h5>
+            <div>
+            { tasks.length !== 0 ? 
+               (
+                    tasks.map((task) => {
+                        return <TaskComponent task={task}/>
+                    })
+                ) : <><p>You are free to go. No issues have been assigned to you!</p></>
+            }
+        </div>
+        </div>
+    )
+}
