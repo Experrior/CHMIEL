05c27bc7556cfceddcb7ee541cae6bcbe1680d8c
Mateusz <m.molenda4@gmail.com>
2024-06-11 11:22:18 +0200

    update tests

--- chmiel/src/test/groovy/com/backend/chmiel/tests/api/UserTest.groovy
+++ chmiel/src/test/groovy/com/backend/chmiel/tests/api/UserTest.groovy
@@ -18,79 +18,114 @@ class UserTest extends Specification {
 
     def "create a new user"() {
         given:
-        User user = new User(firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', password: 'password123', address: '123 Main St', birthDate: new Date(), phoneNumber: '123-456-7890')
+            Random random = new Random();
+            StringBuilder stringBuilder = new StringBuilder(30);
+
+            for (int i = 0; i < 30; i++) {
+                int randomDigit = random.nextInt(10);
+                stringBuilder.append(randomDigit);
+            }
+            String email = 'john.doe'+stringBuilder+'example.com'
+            User user = new User(firstName: 'John', lastName: 'Doe', email: email, password: 'password123', address: '123 Main St', birthDate: new java.sql.Date(1999,12,1), phoneNumber: '123-456-7890')
 
         when:
-        userRepository.save(user)
+            userRepository.save(user)
 
         then:
-        user.id != null
+            user.id != null
 
         cleanup:
-        userRepository.delete(user)
+            userRepository.delete(user)
     }
 
     def "test find all users"() {
         given:
-        User user1 = new User(firstName: 'John', lastName: 'Doe', email: 'john.doe1@example.com', password: 'password123', address: '123 Main St', birthDate: new Date(), phoneNumber: '123-456-7890')
-        User user2 = new User(firstName: 'Jane', lastName: 'Smith', email: 'jane.smith@example.com', password: 'password123', address: '456 Oak St', birthDate: new Date(), phoneNumber: '987-654-3210')
-        userRepository.save(user1)
-        userRepository.save(user2)
+            Integer userCount = userRepository.findAll().size()
+
 
         when:
-        List<User> users = userRepository.findAll()
+            User user1 = new User(firstName: 'John', lastName: 'Doe', email: 'john.doe1@example.com', password: 'password123', address: '123 Main St', birthDate: new java.sql.Date(1999,12,1), phoneNumber: '123-456-7890')
+            User user2 = new User(firstName: 'Jane', lastName: 'Smith', email: 'jane.smith@example.com', password: 'password123', address: '456 Oak St', birthDate: new java.sql.Date(1999,12,1), phoneNumber: '987-654-3210')
+            userRepository.save(user1)
+            userRepository.save(user2)
+            List<User> users = userRepository.findAll()
 
         then:
-        users.size() == 2
-        users.stream().anyMatch {o -> user1.getId() == o.getId()}
-        users.stream().anyMatch {o -> user2.getId() == o.getId()}
+            users.size() == userCount + 2
+            users.stream().anyMatch {o -> user1.getId() == o.getId()}
+            users.stream().anyMatch {o -> user2.getId() == o.getId()}
 
         cleanup:
-        userRepository.delete(user1)
-        userRepository.delete(user2)
+            userRepository.delete(user1)
+            userRepository.delete(user2)
     }
 
     def "test find user by id"() {
         given:
-        User user = new User(firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', password: 'password123', address: '123 Main St', birthDate: new Date(), phoneNumber: '123-456-7890')
-        userRepository.save(user)
+            Random random = new Random();
+            StringBuilder stringBuilder = new StringBuilder(30);
+
+            for (int i = 0; i < 30; i++) {
+                int randomDigit = random.nextInt(10);
+                stringBuilder.append(randomDigit);
+            }
+            String email = 'john.doe'+stringBuilder+'example.com'
+            User user = new User(firstName: 'John', lastName: 'Doe', email: email, password: 'password123', address: '123 Main St', birthDate: new java.sql.Date(1999,12,1), phoneNumber: '123-456-7890')
+            userRepository.save(user)
 
         when:
-        Optional<User> optionalUser = userRepository.findById(user.id)
+            Optional<User> optionalUser = userRepository.findById(user.id)
 
         then:
-        optionalUser.isPresent()
-        optionalUser.get().email == 'john.doe@example.com'
+            optionalUser.isPresent()
+            optionalUser.get().email == email
 
         cleanup:
-        userRepository.delete(user)
+            userRepository.delete(user)
     }
 
     def "test update user"() {
         given:
-        User user = new User(firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', password: 'password123', address: '123 Main St', birthDate: new Date(), phoneNumber: '123-456-7890')
-        userRepository.save(user)
+            Random random = new Random();
+            StringBuilder stringBuilder = new StringBuilder(30);
+
+            for (int i = 0; i < 30; i++) {
+                int randomDigit = random.nextInt(10);
+                stringBuilder.append(randomDigit);
+            }
+            String email = 'john.doe'+stringBuilder+'example.com'
+            User user = new User(firstName: 'John', lastName: 'Doe', email: email, password: 'password123', address: '123 Main St', birthDate: new java.sql.Date(1999,12,1), phoneNumber: '123-456-7890')
+            userRepository.save(user)
 
         when:
-        user.email = 'john.doe.updated@example.com'
-        userRepository.save(user)
+            String newMail = 'john.doe.updated@example.com'+stringBuilder
+            user.email = newMail
+            userRepository.save(user)
 
         then:
-        userRepository.findById(user.id).get().email == 'john.doe.updated@example.com'
+            userRepository.findById(user.id).get().email == newMail
 
         cleanup:
-        userRepository.delete(user)
+            userRepository.delete(user)
     }
 
     def "test delete user"() {
         given:
-        User user = new User(firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', password: 'password123', address: '123 Main St', birthDate: new Date(), phoneNumber: '123-456-7890')
-        userRepository.save(user)
+            Random random = new Random();
+            StringBuilder stringBuilder = new StringBuilder(30);
+
+            for (int i = 0; i < 30; i++) {
+                int randomDigit = random.nextInt(10);
+                stringBuilder.append(randomDigit);
+            }
+            String email = 'john.doe'+stringBuilder+'example.com'
+            User user = new User(firstName: 'John', lastName: 'Doe', email: email, password: 'password123', address: '123 Main St', birthDate: new java.sql.Date(1999,12,1), phoneNumber: '123-456-7890')
+            userRepository.save(user)
 
         when:
-        userRepository.deleteById(user.id)
+            userRepository.deleteById(user.id)
 
         then:
-        !userRepository.findById(user.id).isPresent()
+            !userRepository.findById(user.id).isPresent()
     }
 }
