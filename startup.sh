b1a87043524205899abeced454f3199e855dc5d5
Matthew <73387931+Experrior@users.noreply.github.com>
2024-06-22 21:18:36 +0200

    simplified startup.sh

--- startup.sh
+++ startup.sh
@@ -6,27 +6,11 @@ docker compose -f compose.yaml down
 
 # Build images
 if [[ $# -ge 1 ]] ; then
-
-	docker build -f Dockerfile_front . -t chmiel_front
-	docker build -f Dockerfile_db . -t chmiel_db
-	docker build -f Dockerfile_back . -t chmiel_back
-	docker build -f Dockerfile_py . -t chmiel_py
-	docker build -f Dockerfile_locust . -t chmiel_stress_test
-
+	docker compose -f compose.yaml build
 fi
-
-
-docker rm -f chmiel_back
-docker rm -f chmiel_front
-docker rm -f chmiel_db
-docker rm -f chmiel_py
-docker rm -f chmiel_stress_test
-# Build compose
-docker compose -f compose.yaml build
 
 # Run compose
 docker compose -f compose.yaml up -d --remove-orphans
-
 
 set +e
 watch -n 1 docker ps
